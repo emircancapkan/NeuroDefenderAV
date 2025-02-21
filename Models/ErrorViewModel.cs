33434381c8ddf811b8485b64f87e4ba6f93b3fcf
Emircan Çapkan <99869455+emircancapkan@users.noreply.github.com>
2025-02-21 14:19:54 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NeuroDefenderAV.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
